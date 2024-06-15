ffe9a079b8c89a9c9a5d8c84de50d8c046f85cc3
firman <firmansmoh842@gmail.com>
2024-06-15 21:52:02 +0700

    add : user and question master

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TryoutModule } from './tryout/tryout.module';
+import { QuestionModule } from './question/question.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TryoutModule],
+  imports: [TryoutModule, QuestionModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
