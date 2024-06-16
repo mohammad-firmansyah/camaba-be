55cda97a9af3ebcd39dd591932d1635654db3ad0
firman <firmansmoh842@gmail.com>
2024-06-16 17:14:52 +0700

    add : auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TryoutModule } from './tryout/tryout.module';
 import { QuestionModule } from './question/question.module';
 import { UserModule } from './user/user.module';
 import { HistoryTryoutModule } from './history-tryout/history-tryout.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TryoutModule, QuestionModule, UserModule, HistoryTryoutModule],
+  imports: [TryoutModule, QuestionModule, UserModule, HistoryTryoutModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Camaba-be')
+    .setDescription('The CAMABA API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('camaba')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
